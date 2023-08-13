fa86779b75bf4efda1595804f531d1c1b3b6af81
uikrati <87569719+uikrati@users.noreply.github.com>
2023-08-13 12:14:13 +0530

    first commit

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.get('/api/whoami', function (req, res) {
+  const ip=req.ip;
+  const language=req.headers["accept-language"];
+  const software=req.headers["user-agent"];
+  res.json({
+    ipaddress: ip,
+    language: language,
+    software: software
+  });});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
